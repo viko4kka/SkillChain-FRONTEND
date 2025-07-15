72db1f0c17ea920ee46d858ba9bc479fbfd6dc53
Viktoriia Kaspryk <147540172+viko4kka@users.noreply.github.com>
2025-07-15 13:58:31 +0200

    create initial structure of project and create header with navigation… (#1)
    * create initial structure of project and create header with navigation and styled it
    
    * correct project structure

---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center h-screen"> Not Found</div>
+  );
+}
---
+++ src/components/LogoSkillChain.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function LogoSkillChain() {
+  return (
+    <div>
+      <Image
+        src="/logo-skillchain.svg"
+        height="55"
+        width="55"
+        alt="SkillChain"
+      />
+    </div>
+  );
+}
