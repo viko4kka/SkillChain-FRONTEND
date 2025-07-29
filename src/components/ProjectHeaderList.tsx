5f783a859a7803aded53912fcf3203c7471bf01b
Wiktoria Wachel <wiktoriawachel04@gmail.com>
2025-07-29 13:19:52 +0200

    fixed projectHeader padding

--- src/components/ProjectHeaderList.tsx
+++ src/components/ProjectHeaderList.tsx
@@ -6,8 +6,10 @@ export default function ProjectsHeaderList({
   AddProjectProps?: any;
 }) {
   return (
-    <div className="flex w-full items-center justify-between px-4 pt-5 sm:px-10 lg:px-15">
-      <h1 className="text-dark-text text-xl font-bold md:text-2xl">Projects</h1>
+    <div className="flex w-full items-center justify-between px-4 pt-4 sm:px-10 sm:pt-8 lg:px-15 lg:pt-13">
+      <h1 className="text-dark-text text-xl font-bold sm:text-2xl lg:text-3xl">
+        Projects
+      </h1>
       <div>
         <AddProject {...AddProjectProps} />
       </div>
