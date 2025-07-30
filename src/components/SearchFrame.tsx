e01c68216c9c7419b843a4050c98b2f01883fc18
viko4kka <kasprikviktoria4@gmail.com>
2025-07-30 10:03:14 +0200

    add search structure

--- src/components/SearchFrame.tsx
+++ src/components/SearchFrame.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-import { GoSearch } from "react-icons/go";
 import FilterPanel from "./FilterPanel";
-import Button from "./Button";
+import SearchInput from "./SearchInput";
 
 export default function SearchFrame() {
   return (
@@ -9,24 +7,9 @@ export default function SearchFrame() {
       <h2 className="text-dark-text text-xl font-bold lg:text-2xl">
         Search person
       </h2>
-      <div className="border-dark-text/20 mt-6 flex w-full items-center rounded-sm border px-1 transition">
-        <span className="text-dark-text/40 text- flex-shrink-0">
-          <GoSearch />
-        </span>
-        <input
-          type="text"
-          className="text-dark-text/60 w-full bg-transparent px-1 py-2 text-sm focus:outline-none"
-          placeholder="Search person by first name or last name..."
-        />
-      </div>
-      <div>
-        <FilterPanel />
-      </div>
-      <div className="mt-6">
-        <Button variant="primary" size="md" fullWidth>
-          Search
-        </Button>
-      </div>
+
+      <SearchInput />
+      <FilterPanel />
     </div>
   );
 }
