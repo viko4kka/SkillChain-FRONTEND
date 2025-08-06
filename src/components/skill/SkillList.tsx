8151517cb11e5c54fc30ccb0508857391f3b172e
Wiktoria Wachel <wiktoriawachel04@gmail.com>
2025-08-06 09:46:09 +0200

    added - no skill found

--- src/components/skill/SkillList.tsx
+++ src/components/skill/SkillList.tsx
@@ -7,6 +7,7 @@ import { useStore } from "@/stores/useStore";
 import useSkillsByUserId from "@/hooks/useSkillByUserId";
 import SkillsListHeader from "./SkillsListHeader";
 import SkillCard from "./SkillCard";
+import { FiStar } from "react-icons/fi";
 
 interface SkillListProps {
   userId: number;
@@ -35,7 +36,15 @@ const SkillList: React.FC<SkillListProps> = ({ userId }) => {
         </div>
       )}
       {!isLoading && (!skills || skills.length === 0) && (
-        <div>No skills found.</div>
+        <div className="flex flex-col items-center justify-center gap-2 py-10 text-gray-500">
+          <FiStar className="text-mainBlue mb-2 text-4xl" />
+          <span className="text-lg font-medium">No skills found</span>
+          {canEdit && (
+            <span className="text-sm text-gray-400">
+              Add your first skill using the <b>+</b> button above!
+            </span>
+          )}
+        </div>
       )}
       {!isLoading && skills && skills.length > 0 && (
         <>
